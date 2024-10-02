89114a718a3efe0e41179a28a696140baf582592
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-02 12:43:02 +0000

    implement menu items and import navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,18 +11,24 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* Left sidebar */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
         <Link
           href="/"
-          className="flex items-center justify-center lg:justify-start gap-2"
+          className="flex items-center justify-center lg:justify-start p-4 gap-2"
         >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">G.I.A Prep Sch.</span>
         </Link>
+
+        {/* Menu */}
+        <Menu />
       </div>
 
       {/* Right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
