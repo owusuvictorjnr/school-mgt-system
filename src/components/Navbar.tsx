9a7bbf8c2e3ab5341e9b74cc1115f37bad3b48e6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-02 12:45:42 +0000

    navbar implementation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex items-center justify-between p-4">
+      {/* Search Bar */}
+      <div className="hidden md:flex">
+        <Image src={"/search.png"} alt="search" width={14} height={14} />
+        <input
+          type="text"
+          placeholder="Search"
+          className="border-none bg-transparent ml-2"
+        />
+      </div>
 
-export default Navbar
+      {/* Icons & Users */}
+      <div className="flex items-center gap-6">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src={"/message.png"} alt="message" width={20} height={20} />
+        </div>
+
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image
+            src={"/announcement.png"}
+            alt="announcement"
+            width={20}
+            height={20}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <span className="text-sm leading-3 font-medium">Vitech</span>
+          <span className="">Admin</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
