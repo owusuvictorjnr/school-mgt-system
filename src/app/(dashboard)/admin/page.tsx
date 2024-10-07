396725a2676ef7e0d36fe8a87777eb7964f251b5
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-07 17:04:11 +0000

    Admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,41 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User Cards */}
+        <div className="flex justify-between gap-4 flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+
+        {/* Middle Chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+
+        {/* Bottom Chart */}
+        <div className=""></div>
+      </div>
+
+      {/* Right */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
