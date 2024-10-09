ec68ff71e7122064e1b8787687d6b329bb2b997a
owusuvictorjnr <owusuvictor80@gmail.com>
2024-10-09 17:49:50 +0000

    parents list page

---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,133 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { role, studentsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type Student = {
+  id: number;
+  studentId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone?: string;
+  grade: number;
+  class: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+
+  {
+    header: "Student ID",
+    accessor: "studentid",
+    className: "hidden md:table-cell",
+  },
+
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const ParentListPage = () => {
+  const renderRow = (item: Student) => (
+    <tr
+      key={item.class}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-vPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.photo}
+          alt={item.name}
+          width={10}
+          height={10}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item.class}</p>
+        </div>
+      </td>
+
+      <td className="hidden md:table-cell">{item.studentId}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="">
+        <div className="flex items-center gap-2">
+          <Link href={`/list/student/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-skyBlue">
+              <Image src="/view.png" width={16} height={16} alt="view" />
+            </button>
+          </Link>
+
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-vPurple">
+              <Image src="/delete.png" width={16} height={16} alt="delete" />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
+        <div className="flex flex-col md:flow-row items-center gap-4 w-full md:w-auto">
+          <div className="flex items-center gap-4   self-end">
+            <TableSearch />
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/filter.png" width={14} height={14} alt="filter" />
+            </button>
+
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/sort.png" width={14} height={14} alt="sort" />
+            </button>
+
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                <Image src="/plus.png" width={14} height={14} alt="plus" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* List */}
+      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+
+      {/* Pagination */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default ParentListPage;
